2b58f3b0541530fce111dfc176e9d9abd6c613dd
Kaewdisorn <kds.napha@gmail.com>
2021-11-13 21:24:13 +0900

    Create request

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Kaewdisorn
+
+go 1.17
--- main.go
+++ main.go
@@ -2,9 +2,23 @@ package main
 
 import (
 	"fmt"
+	"net/http" // import package net-http
 )
 
 func main() {
 
-	fmt.Println("hello world")
+	handlerequest()
+}
+
+// Create method for client request
+
+func homePage(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprint(w, "Welcome to the HomePage!")
+}
+
+func handlerequest() {
+
+	http.HandleFunc("/", homePage)
+	http.ListenAndServe(":8080", nil)
 }
